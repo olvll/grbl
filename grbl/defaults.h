4d4e100b83e0fd2a716b9e465578d2166d9c21d3
olvll <46418322+olvll@users.noreply.github.com>
2020-05-03 12:31:57 +0200

    Added defaults for MX1100 series.

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -529,6 +529,44 @@
   #define DEFAULT_HOMING_PULLOFF 1.0 // mm
 #endif
 
+#ifdef DEFAULTS_MX1100
+  // Settings for mini CNC developed by FabLab Midden-Limburg
+  #define DEFAULT_X_STEPS_PER_MM 533.333 // 200 * 8 / 3
+  #define DEFAULT_Y_STEPS_PER_MM 533.333 // 200 * 8 / 3
+  #define DEFAULT_Z_STEPS_PER_MM 533.333 // 200 * 8 / 3
+  #define DEFAULT_X_MAX_RATE 2500.0 // mm/min
+  #define DEFAULT_Y_MAX_RATE 2500.0 // mm/min
+  #define DEFAULT_Z_MAX_RATE 500.0 // mm/min
+  #define DEFAULT_X_ACCELERATION (30.0*60*60) // 30 mm/sec^2
+  #define DEFAULT_Y_ACCELERATION (30.0*60*60) // 30 mm/sec^2
+  #define DEFAULT_Z_ACCELERATION (30.0*60*60) // 30 mm/sec^2
+  #define DEFAULT_X_MAX_TRAVEL 260.0 // mm
+  #define DEFAULT_Y_MAX_TRAVEL 310.0 // mm
+  #define DEFAULT_Z_MAX_TRAVEL 120.0 // mm
+  #define DEFAULT_SPINDLE_RPM_MAX 1.0 // rpm for use with on/off relay
+  #define DEFAULT_SPINDLE_RPM_MIN 0.0
+  #define DEFAULT_STEP_PULSE_MICROSECONDS 5 // default = 10
+  #define DEFAULT_STEPPING_INVERT_MASK 7 // step on low
+  #define DEFAULT_DIRECTION_INVERT_MASK 3 // x and y axis inverted
+  #define DEFAULT_STEPPER_IDLE_LOCK_TIME 255 // 255 keeps steppers enabled
+  #define DEFAULT_STATUS_REPORT_MASK 1 // MPos enabled
+  #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
+  #define DEFAULT_ARC_TOLERANCE 0.002 // mm
+  #define DEFAULT_REPORT_INCHES 0 // false
+  #define DEFAULT_INVERT_ST_ENABLE 1 // true
+  #define DEFAULT_INVERT_LIMIT_PINS 0 // false
+  #define DEFAULT_SOFT_LIMIT_ENABLE 1 // true
+  #define DEFAULT_HARD_LIMIT_ENABLE 1  // true
+  #define DEFAULT_INVERT_PROBE_PIN 0 // false
+  #define DEFAULT_LASER_MODE 0 // false
+  #define DEFAULT_HOMING_ENABLE 1  // true
+  #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir
+  #define DEFAULT_HOMING_FEED_RATE 100.0 // mm/min
+  #define DEFAULT_HOMING_SEEK_RATE 1000.0 // mm/min
+  #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
+  #define DEFAULT_HOMING_PULLOFF 1.0 // mm
+#endif
+
 #ifdef DEFAULTS_SIMULATOR
   // Settings only for Grbl Simulator (www.github.com/grbl/grbl-sim)
   // Grbl generic default settings. Should work across different machines.
